b25fd696369c7505b0d2332aaa3622442715c144
harikrishnan a <harikrishnana@datamatica.uk>
2023-12-01 18:01:27 +0530

    Add project files.

---
+++ MobAppXamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MobAppXamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MobAppXamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MobAppXamarin/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MobAppXamarin.Models;
+using MobAppXamarin.ViewModels;
+using MobAppXamarin.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobAppXamarin.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
